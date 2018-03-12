95b1280c436826f5187072bee2056722704dc9bd
luisgumucio <luisgumucioflores@gmail.com>
2018-03-12 06:51:43 -0400

    grunt add

---
+++ Gruntfile.js
@@ -0,0 +1,14 @@
+module.exports = function (grunt) {
+    grunt.initConfig({
+        jshint: {
+            files: ['Gruntfile.js'],
+            options: {
+                globals: {
+                    jQuery: true
+                }
+            }
+        }
+    });
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.registerTask('default', ['jshint']);
+};
